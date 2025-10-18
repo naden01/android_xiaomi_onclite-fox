504d41ac9ebdc7e7ebfd736a46a4fad6b01fc1e6
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-10-18 01:47:46 -0700

    experiment

--- device.mk
+++ device.mk
@@ -13,6 +13,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 TARGET_SCREEN_HEIGHT := 1520
 TARGET_SCREEN_WIDTH := 720
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.boot.dynamic_partitions=true \
+	ro.boot.dynamic_partitions_retrofit=true
+
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.fastbootd.available=true
+
 # Crypto
 PRODUCT_PACKAGES += qcom_decrypt_fbe
 PRODUCT_PACKAGES += qcom_decrypt
